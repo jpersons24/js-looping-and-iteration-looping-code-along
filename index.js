10a4804bd14e658d306f55d12b607fb83ce79832
Jakob Persons <jakobep24@gmail.com>
2021-01-10 11:49:44 -0500

    commented things out

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // Code your solutions in this file
 
-for (let age = 30; age < 40; age++) {
-   console.log(`I'm ${age} years old. Happy birthday to me!`);
-}
+// for (let age = 30; age < 40; age++) {
+//    console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
 
-const gifts = ['teddy bear', 'drone', 'doll'];
+// const gifts = ['teddy bear', 'drone', 'doll'];
 
-function wrapGifts (gifts) {
-   for (let i = 0; i < gifts.length; i++) {
-      console.log(`Wrapped ${gifts[i]} and added a bow!`);
-   }
-   return gifts;
-}
+// function wrapGifts (gifts) {
+//    for (let i = 0; i < gifts.length; i++) {
+//       console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//    }
+//    return gifts;
+// }
 
-wrapGifts(gifts);
+// wrapGifts(gifts);
 
 const names = ['Lisa', 'Kaitlin', 'Jan']
 const event = 'surprise'
